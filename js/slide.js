b73da30b29368ccd387d3e31c07c2482c0c6e3c1
cezarcalixto <cezarcalixto222@gmail.com>
2021-02-18 18:41:06 -0300

    esqueleto dos eventos

--- js/slide.js
+++ js/slide.js
@@ -5,18 +5,21 @@ export default class Slide {
     }
     onStart(event){
         event.preventDefault();
-        console.log(this)
+        this.wrapper.addEventListener('mousemove', this.onMove);
     }
     onMove(event){
-        console.log('moveu')
+    }
+    onEnd(event){
+        this.wrapper.removeEventListener('mousemove', this.onMove);
     }
     addSlideEvent(){
         this.wrapper.addEventListener('mousedown', this.onStart);
-        this.wrapper.addEventListener('mousemove', this.onMove);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
     }
     bindEvents(){
         this.onStart = this.onStart.bind(this)
         this.onMove = this.onMove.bind(this)
+        this.onEnd = this.onEnd.bind(this)
     }
     init(){
         this.bindEvents();
