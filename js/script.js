0b8903df7516b2365776788684498201e522c402
cezarcalixto <cezarcalixto222@gmail.com>
2021-02-21 16:49:39 -0300

    adicionada paginação por Slide

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ import {Slide, SlideNav} from './slide.js';
 const slide = new SlideNav ('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.next', '.prev');
+slide.addControl();
 console.log(slide)
